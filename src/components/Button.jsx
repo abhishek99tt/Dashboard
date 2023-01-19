14f455552ef0ca3cd9243f71a3b6b0afa17c4888
Abhishek Singh <abhishek.s@codilar.com>
2023-01-19 08:43:44 +0530

    HomePage Section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ color, bgColor, size, text, borderRadius }) => {
   return (
-    <div>Button</div>
+    <button
+      type='button'
+      style={{ color, background:bgColor, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
   )
 }
 
