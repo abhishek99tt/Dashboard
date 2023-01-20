7a330266d8204eb7bfbed3c4e2eafe221dba685f
Abhishek Singh <abhishek.s@codilar.com>
2023-01-20 09:13:54 +0530

    Revenue Section completed using syncfusion charts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -57,6 +57,84 @@ const Ecommerce = () => {
 
       </div>
 
+      {/* Revenue Section */}
+      <div className='flex gap-10 justify-center flex-wrap'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-3 rounded-2xl md:w-780'>
+              <div className='flex justify-between'>
+                <p className='font-seminbold text-xl'>Revenue Section</p>
+                <div className='flex items-center gap-4'>
+                  <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                    <span><GoPrimitiveDot/> </span>
+                    <span>Expense</span>
+                  </p>
+                  <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                    <span><GoPrimitiveDot/> </span>
+                    <span>Budget</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className='flex gap-10 mt-10 flex-wrap justify-center'>
+                <div className='border-r-1 border-color m-4 pr-10'>
+                  {/* Budget */}
+                  <div>
+                    <p>
+                      <span className='text-3xl font-semibold'>
+                      $93,438
+                      </span>
+                      <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full bg-green-400 text-white m-3 text-xs'>
+                        23%
+                      </span>
+                    </p>
+                    <p className='text-gray-500 mt-1'>Budget</p>
+                  </div>
+                  {/* Expense */}
+                  <div className='mt-8'>
+                    <p>
+                      <span className='text-3xl font-semibold'>
+                      $48,487
+                      </span>
+                    </p>
+                    <p className='text-gray-500 mt-1'>Expense</p>
+                  </div>
+
+                  {/* Sparkline Chart */}
+                  <div className='mt-5'>
+                    <SparkLine
+                      currentColor="blue"
+                      id="line-sparkline"
+                      type="Line"
+                      height="80px"
+                      data={SparklineAreaData}
+                      color="blue"
+                    />
+                  </div>
+
+                  {/* Budget */}
+                  <div className='mt-10'>
+                    <Button
+                      color="white"       
+                      bgColor="blue"
+                      size=""
+                      text="Download Report"
+                      borderRadius="10px"
+                    />
+                  </div>
+
+                </div>
+
+                <div>
+                  <Stacked
+                    width="320px"
+                    height="360px"
+                  />
+                </div>
+
+              </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
