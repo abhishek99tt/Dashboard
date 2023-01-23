4cd8f857bd28fb27d234a6c44ec7656639a1a674
Abhishek Singh <abhishek.s@codilar.com>
2023-01-23 07:43:29 +0530

    Theme and app part completed

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import './App.css'
 import { useStateContext } from './context/ContextProvider';
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
   // const activeMenu = true;
   return (
-    <>
+    <div className={ currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
             <div className='fixed right-4 bottom-4'style={{ zIndex: 1000 }}>
-              <TooltipComponent content="Settings" position="Top">
-                <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ backgroundColor:'blue', borderRadius:'50%'}}>
+              <TooltipComponent content="Settings" position="Top"  >
+                <button type='button' onClick={() => setThemeSettings(true)} className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ backgroundColor:currentColor, borderRadius:'50%'}}>
                   <FiSettings/> 
                 </button>
               </TooltipComponent>
@@ -31,12 +31,16 @@ const App = () => {
                 </div>
             )}
             <div className={ 
-              `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72' : 'flex-2'}`
+              `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+              ${ activeMenu ? 
+                'md:ml-72' :
+                'flex-2'}`
              }>
               <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                 <Navbar/>
 
               <div>
+               { themeSettings && <ThemeSettings  /> }
                 <Routes>
                   {/* Dashboard */}
                   <Route path='/' element={<Ecommerce/>} />
@@ -70,7 +74,7 @@ const App = () => {
 
         </div>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
